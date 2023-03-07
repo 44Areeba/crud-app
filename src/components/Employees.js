c96e6d4a76688e35ea83b1c1237ca408d86c07a0
Areeba_Khan44 <98646330+44Areeba@users.noreply.github.com>
2023-03-07 17:10:12 +0500

    crud app
    edit update delete

---
+++ src/components/Employees.js
@@ -0,0 +1,40 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Areeba",
+        Age:"10",
+
+    },
+    {
+        id:"2",
+        Name:"Mohib",
+        Age:"15",
+
+    },
+    {
+        id:"3",
+        Name:"Maryam",
+        Age:"12",
+
+    },
+    {
+        id:"4",
+        Name:"Wardah",
+        Age:"23",
+
+    },
+    {
+        id:"5",
+        Name:"Ansar",
+        Age:"24",
+
+    },
+    {
+        id:"6",
+        Name:"Abdullah",
+        Age:"25",
+
+    },
+]
+
+export default Employees;
